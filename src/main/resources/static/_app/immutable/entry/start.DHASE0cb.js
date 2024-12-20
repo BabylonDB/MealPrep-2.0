400cdcbf01dad3a3fb9704ece6bbd3b2c7fabde7
baskaber <baskaber*@students.zhaw.ch>
2024-12-20 12:18:43 +0100

    Frontend verschönerungen

--- src/main/resources/static/_app/immutable/entry/start.DHASE0cb.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.NwXBpVuM.js";export{t as start};
-//# sourceMappingURL=start.DHASE0cb.js.map
---
+++ src/main/resources/static/_app/immutable/entry/start.Q3rrJJiX.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.B65FAxKc.js";export{t as start};
+//# sourceMappingURL=start.Q3rrJJiX.js.map
